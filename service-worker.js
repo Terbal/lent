43801157ce0028191e7e8489a6b6e1fb5fd38375
terbal <joelmoyo249@gmail.com>
2025-06-12 12:34:56 +0100

    Monitor et service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,21 @@
 // service-worker.js
 const CACHE_NAME = "lent-v1";
+const ASSETS = [
+  "./", // équivaut à /lent/
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) =>
-        cache.addAll(["/", "/index.html", "/app.js", "/style.css"])
-      )
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
